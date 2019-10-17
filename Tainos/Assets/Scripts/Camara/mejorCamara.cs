c5719aec2da046572ddac78e72b9e16d24bd5bdd
JeanG <jean18699@gmail.com>
2019-10-17 19:12:36 -0400

    agregado sprite
    se agrego el sprite choza1

---
+++ Tainos/Assets/Scripts/Camara/mejorCamara.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class mejorCamara : MonoBehaviour
+{
+
+    public Transform target;
+  
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.position = new Vector3(target.position.x, target.position.y,-10f);      
+    }
+
+    private void LateUpdate()
+    {
+       
+
+    }
+
+
+
+}
+
+
+
